45f6228704f7ef4e00d17d8136e8cabce7d0cc3a
Lisa <lisaphysik@gmail.com>
2020-05-01 14:41:08 +0200

    Initial Unreal Project

---
+++ Source/SaveThePlanets/SaveThePlanetsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SaveThePlanetsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAVETHEPLANETS_API ASaveThePlanetsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
